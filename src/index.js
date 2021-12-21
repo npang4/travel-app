4f45cc1e9778191048d002823d9de7c83ed9bc67
Nelson Pang <nelsoncpang@gmail.com>
2021-12-20 18:00:00 -0800

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import  ReactDOM  from 'react';
+import  ReactDOM  from 'react-dom';
 
-const element = <h1>Hello world</h1>;
-console.log(element);  
+
+const element = <h1> Hello world</h1>;
+ReactDOM.render(element, document.getElementById('root'));
